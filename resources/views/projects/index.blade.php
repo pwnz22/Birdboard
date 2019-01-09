513d98e045964e8508a8728684d32259aab98ed5
Valijon <parliament888@gmail.com>
2019-01-09 17:45:38 +0500

    Beginning

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+<h1>Birdboard</h1>
+
+<ul>
+    @foreach($projects as $project)
+            <li>{{ $project->title }}</li>
+    @endforeach
+</ul>
+</body>
+</html>
