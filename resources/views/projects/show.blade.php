018a95a4c304eede554ad37ad2f8e87e5d53ccb1
Valijon <parliament888@gmail.com>
2019-01-10 11:51:08 +0500

    Model Tests (Feature)

---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+    <h1>{{ $project->title }}</h1>
+    <div>{{ $project->description }}</div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
 
 Route::get('/projects', 'ProjectsController@index');
+Route::get('/projects/{project}', 'ProjectsController@show');
 Route::post('/projects', 'ProjectsController@store');
