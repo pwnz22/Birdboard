12b1f4a4d62bd39dd5221cde22ff821fb915062b
Valijon <parliament888@gmail.com>
2019-03-14 18:17:13 +0500

    Scoping Projects

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,9 @@
 <?php
-
-Route::get('/projects', 'ProjectsController@index');
-Route::get('/projects/{project}', 'ProjectsController@show');
-Route::post('/projects', 'ProjectsController@store')->middleware('auth');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/projects', 'ProjectsController@index');
+    Route::get('/projects/{project}', 'ProjectsController@show');
+    Route::post('/projects', 'ProjectsController@store');
+});
 
 Auth::routes();
 
